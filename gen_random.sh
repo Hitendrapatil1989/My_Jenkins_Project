3d7787752f9f37a6f7f7d21615112122364d2f2c
Hitendra <hitendrapatil1@gmail.com>
2021-07-13 19:18:12 +0530

    sum via function

--- gen_random.sh
+++ gen_random.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-echo "Script started ...!!"
-echo $RANDOM
 
 sum(){
     result=`expr $1 + $2`
     echo "Sum is " $result
 }
-
+if [ $# -ne 2];then
+    echo "Please pass correct number of parameters ..!!"
+else
+echo "inside function loop ..!!"
 sum $num1 $num2
-
 echo "Script completed ..!!"
-
+fi
 
