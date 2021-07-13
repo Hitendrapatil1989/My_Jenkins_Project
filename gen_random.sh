70b9a5dcc5f8e684f20fdfdbe83692181e0b5a74
Hitendra <hitendrapatil1@gmail.com>
2021-07-14 00:06:00 +0530

    function change

--- gen_random.sh
+++ gen_random.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 sum(){
-    result=`expr $1 + $2`
+    result=$(($1+$2))
     echo "Sum is " $result
 }
 if [ $# -ne 2 ];then
     echo "Please pass correct number of parameters ..!!"
 else
 echo "inside function loop ..!!"
-sum $num1 $num2
+sum $1 $2
 echo "Script completed ..!!"
 fi
-
+#num1=$1
+#num2=$2
+#sum=`expr $num1 + $num2`
+#echo $sum
